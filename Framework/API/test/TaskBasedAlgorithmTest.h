7745ce69405e36b35905448abb96452e9a452ce2
Mial Lewis <mial.lewis@stfc.ac.uk>
2026-04-28 11:13:24 +0100

    add task based alg test

--- Framework/API/test/TaskBasedAlgorithmTest.h
+++ Framework/API/test/TaskBasedAlgorithmTest.h
@@ -8,9 +8,14 @@
 
 #include <cxxtest/TestSuite.h>
 
+#include "MantidAPI/AlgorithmFactory.h"
 #include "MantidAPI/FrameworkManager.h"
 #include "MantidAPI/TaskBasedAlgorithm.h"
-// #include "MantidAPI/AnalysisDataService.h"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
 
 namespace {
 class ToyTaskBasedAlg : public Mantid::API::TaskBasedAlgorithm<ToyTaskBasedAlg> {
@@ -18,8 +23,19 @@ public:
   const std::string name() const override { return "ToyTaskBasedAlg"; };
   const std::string summary() const override { return "Toy alg for task based algorithm test"; }
   int version() const override { return 1; };
-  void init() override {};
-  void exec() override {};
+  void init() override {
+    declareProperty(std::make_unique<Mantid::API::WorkspaceProperty<Mantid::API::MatrixWorkspace>>(
+                        "InputWorkspace", "", Mantid::Kernel::Direction::Input),
+                    "The input workspace to be passed to the first task");
+    declareProperty(std::make_unique<Mantid::API::WorkspaceProperty<Mantid::API::MatrixWorkspace>>(
+                        "OutputWorkspace", "", Mantid::Kernel::Direction::Output),
+                    "Output workspace from final task");
+    initTaskBasedAlgorithm<TaskA, TaskB, TaskC, TaskD>();
+  };
+  void exec() override {
+    configureAlgorithmTasks();
+    execTasks();
+  };
 
   // Possible orders tested:
   // 1) A, B, C, D
@@ -29,7 +45,11 @@ public:
   class TaskA final : public AlgorithmTask {
   public:
     explicit TaskA(ToyTaskBasedAlg *parent) : AlgorithmTask(parent, "TaskA") { setExpectedOutputs({"TaskAOutput"}); }
-    void executeImpl() override {};
+    void executeImpl() override {
+      auto inputWS = getDependantWorkspace("InputWorkspace");
+      m_parent->scaleWorkspace(inputWS, 5.0);
+      outputWorkspace(inputWS, "TaskAOutput");
+    };
   };
 
   class TaskB final : public AlgorithmTask {
@@ -40,7 +60,15 @@ public:
       auto depSet = addDependantTaskSet();
       setDependantTask("TaskC", "TaskCOutput", "InputWorkspace", depSet);
     }
-    void executeImpl() override {};
+    void executeImpl() override {
+      auto inputWS = getDependantWorkspace("InputWorkspace");
+      auto cloneWS1 = m_parent->cloneWorkspace(inputWS);
+      m_parent->translateWorkspace(cloneWS1, 2.5);
+      auto cloneWS2 = m_parent->cloneWorkspace(inputWS);
+      m_parent->translateWorkspace(cloneWS2, 1.25);
+      outputWorkspace(cloneWS1, "TaskBOutput1");
+      outputWorkspace(cloneWS2, "TaskBOutput2");
+    };
   };
 
   class TaskC final : public AlgorithmTask {
@@ -51,7 +79,11 @@ public:
       auto depSet = addDependantTaskSet();
       setDependantTask("TaskA", "TaskAOutput", "InputWorkspace", depSet);
     }
-    void executeImpl() override {};
+    void executeImpl() override {
+      auto inputWS = getDependantWorkspace("InputWorkspace");
+      m_parent->scaleWorkspace(inputWS, 1.0 / 3.0);
+      outputWorkspace(inputWS, "TaskCOutput");
+    };
   };
 
   class TaskD final : public AlgorithmTask {
@@ -61,10 +93,68 @@ public:
       setDependantTask("TaskC", "TaskCOutput", "InputWorkspace");
       auto depSet = addDependantTaskSet();
       setDependantTask("TaskB", "TaskBOutput2", "InputWorkspace", depSet);
+      setSelectedOutput("TaskDOutput2");
     }
-    void executeImpl() override {};
+    void executeImpl() override {
+      auto inputWS = getDependantWorkspace("InputWorkspace");
+      auto cloneWS1 = m_parent->cloneWorkspace(inputWS);
+      m_parent->translateWorkspace(cloneWS1, -2.50);
+      auto cloneWS2 = m_parent->cloneWorkspace(inputWS);
+      m_parent->translateWorkspace(cloneWS2, -3.75);
+      outputWorkspace(cloneWS1, "TaskDOutput1");
+      outputWorkspace(cloneWS2, "TaskDOutput2");
+    };
   };
+
+  Mantid::API::MatrixWorkspace_sptr cloneWorkspace(const Mantid::API::MatrixWorkspace_sptr &ws) {
+    auto cloneWorkspace = createChildAlgorithm("CloneWorkspace");
+    cloneWorkspace->setProperty("InputWorkspace", ws);
+    cloneWorkspace->execute();
+    Mantid::API::Workspace_sptr wsClone = cloneWorkspace->getProperty("OutputWorkspace");
+    return std::dynamic_pointer_cast<Mantid::API::MatrixWorkspace>(wsClone);
+  }
+
+  void scaleWorkspace(Mantid::API::MatrixWorkspace_sptr &ws, const double val) {
+    auto &y = ws->mutableY(0);
+    for (size_t i = 0; i < y.size(); ++i) {
+      y[i] = y[i] * val;
+    }
+  }
+
+  void translateWorkspace(Mantid::API::MatrixWorkspace_sptr &ws, const double val) {
+    auto &y = ws->mutableY(0);
+    for (size_t i = 0; i < y.size(); ++i) {
+      y[i] = y[i] + val;
+    }
+  }
 };
+
+Mantid::API::MatrixWorkspace_sptr makeMatrixWorkspaceFromVector(const std::vector<double> &yValues,
+                                                                const std::string &outputName = "tmp_ws") {
+  const auto nBins = yValues.size();
+  auto alg = Mantid::API::AlgorithmManager::Instance().create("CreateSampleWorkspace");
+  alg->initialize();
+  alg->setProperty("WorkspaceType", "Histogram");
+  alg->setProperty("NumBanks", 1);
+  alg->setProperty("BankPixelWidth", 1);
+  alg->setProperty("XMax", static_cast<double>(nBins));
+  alg->setProperty("BinWidth", 1.0);
+  alg->setProperty("OutputWorkspace", outputName);
+  alg->setAlwaysStoreInADS(false);
+  alg->execute();
+  Mantid::API::MatrixWorkspace_sptr ws = alg->getProperty("OutputWorkspace");
+
+  auto &x = ws->mutableX(0);
+  auto &y = ws->mutableY(0);
+  auto &e = ws->mutableE(0);
+  for (size_t i = 0; i < x.size(); ++i) {
+    x[i] = static_cast<double>(i);
+  }
+  std::copy(yValues.begin(), yValues.end(), y.begin());
+  std::fill(e.begin(), e.end(), 0.0);
+  return ws;
+}
+
 } // namespace
 
 class TaskBasedAlgorithmTest : public CxxTest::TestSuite {
@@ -82,10 +172,59 @@ public:
 
   ~TaskBasedAlgorithmTest() override { Mantid::API::AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm", 1); }
 
-  void setUp() override {} // AnalysisDataService::Instance().clear(); }
+  void setUp() override { m_alg = Mantid::API::AlgorithmManager::Instance().create("ToyTaskBasedAlg"); }
 
-  void testAlgorithm() {}
+  void tearDown() override {
+    // AnalysisDataService::Instance().clear();
+  }
+
+  void compareVectors(const std::vector<double> &vec1, const std::vector<double> &vec2) {
+    if (vec1.size() != vec2.size()) {
+      TSM_ASSERT("compareVectors: Vector sizes not equal", false);
+      return;
+    }
+
+    for (size_t i = 0; i < vec1.size(); i++) {
+      TS_ASSERT_DELTA(vec1[i], vec2[i], 0.001);
+    }
+  }
+
+  void testTaskBasedAlgorithmABCD() {
+    m_alg->initialize();
+    m_alg->setAlwaysStoreInADS(false);
+    m_alg->setProperty("TaskExecutionOrder", "TaskA, TaskB, TaskC, TaskD");
+    Mantid::API::MatrixWorkspace_sptr inputWS = makeMatrixWorkspaceFromVector({1.0, 2.0, 3.0, 4.0, 5.0});
+    m_alg->setProperty("InputWorkspace", inputWS);
+    m_alg->setProperty("OutputWorkspace", "test_ws");
+    m_alg->execute();
+    Mantid::API::MatrixWorkspace_sptr outputWS = m_alg->getProperty("OutputWorkspace");
+    compareVectors(outputWS->readY(0), {-1.25, 0.4167, 2.0833, 3.75, 5.4167});
+  }
+
+  void testTaskBasedAlgorithmACBD() {
+    m_alg->initialize();
+    m_alg->setAlwaysStoreInADS(false);
+    m_alg->setProperty("TaskExecutionOrder", "TaskA, TaskC, TaskB, TaskD");
+    Mantid::API::MatrixWorkspace_sptr inputWS = makeMatrixWorkspaceFromVector({1.0, 2.0, 3.0, 4.0, 5.0});
+    m_alg->setProperty("InputWorkspace", inputWS);
+    m_alg->setProperty("OutputWorkspace", "test_ws");
+    m_alg->execute();
+    Mantid::API::MatrixWorkspace_sptr outputWS = m_alg->getProperty("OutputWorkspace");
+    compareVectors(outputWS->readY(0), {-0.8333, 0.8333, 2.5, 4.1667, 5.8333});
+  }
+
+  void testTaskBasedAlgorithmABD() {
+    m_alg->initialize();
+    m_alg->setAlwaysStoreInADS(false);
+    m_alg->setProperty("TaskExecutionOrder", "TaskA, TaskB, TaskD");
+    Mantid::API::MatrixWorkspace_sptr inputWS = makeMatrixWorkspaceFromVector({1.0, 2.0, 3.0, 4.0, 5.0});
+    m_alg->setProperty("InputWorkspace", inputWS);
+    m_alg->setProperty("OutputWorkspace", "test_ws");
+    m_alg->execute();
+    Mantid::API::MatrixWorkspace_sptr outputWS = m_alg->getProperty("OutputWorkspace");
+    compareVectors(outputWS->readY(0), {2.50, 7.50, 12.50, 17.50, 22.50});
+  }
 
 private:
-  ToyTaskBasedAlg alg;
+  Mantid::API::IAlgorithm_sptr m_alg;
 };
